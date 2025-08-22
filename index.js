d1e332f8dfbc35f001b4c18b8541d972e6a3d0c1
messaoudssed <32521818+messaoudssed@users.noreply.github.com>
2025-08-22 20:45:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import 'dotenv/config'
 import mongoose from 'mongoose'
 import app from './src/app.js'
+import cors from "cors";
 
 const PORT = process.env.PORT || 5000
 const MONGO_URI = process.env.MONGO_URI
+
+const corsOptions = {
+  origin: "https://client-umber-eta.vercel.app/", 
+  methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+  credentials: true, 
+};
+
+app.use(cors(corsOptions));
 
 async function start() {
   try {
